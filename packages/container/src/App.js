687adbb48b2622d8ccf74f47c07bb9bd7729108c
Skip <skoppensboer@gmail.com>
2021-01-02 14:50:37 +0200

    Updated App to use header

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import { BrowserRouter } from 'react-router-dom'
+import Header from './Components/Header'
 import MarketingApp from './Components/MarketingApp'
 
 export default () => {
-    return <div>
-        <h1>Container App!</h1>
-        <hr />
-        <MarketingApp />
-    </div>
+    return (
+        <BrowserRouter>
+        <div>
+            <Header />
+            <MarketingApp />
+        </div>
+        </BrowserRouter>
+    )
 }
