be0987bcea97073e6067cb462a80f19494dc400c
Skip <skoppensboer@gmail.com>
2021-01-02 11:10:28 +0200

    Added cache invalidation to workflow

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,6 +4,7 @@ import MarketingApp from './Components/MarketingApp'
 export default () => {
     return 
     <div>
+        <h1>Container App</h1>
         <hr />
         <MarketingApp />
     </div>
