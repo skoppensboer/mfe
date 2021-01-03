693ea35020bd914ee7d563a56cdb22390c5349b3
Skip <skoppensboer@gmail.com>
2021-01-03 15:32:42 +0200

    Added login and logout functionality

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,4 +1,4 @@
-import React, { lazy, Suspense } from 'react'
+import React, { lazy, Suspense, useState } from 'react'
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import { StylesProvider, createGenerateClassName } from '@material-ui/styles'
 import Header from './Components/Header'
@@ -13,16 +13,20 @@ const generateClassName = createGenerateClassName({
 })
 
 export default () => {
+    const [isSignedIn, setIsSignedIn] = useState(false)
+
     return (
         <BrowserRouter>
             <StylesProvider generateClassName={generateClassName}>
                 <div>
-                    <Header />
+                    <Header onSignOut={() => setIsSignedIn(false)} isSignedIn={isSignedIn} />
                     <Suspense fallback={<Progress />}>
-                    <Switch>
-                        <Route path="/auth" component={AuthAppLazy} />
-                        <Route path="/" component={MarketingAppLazy} />
-                    </Switch>
+                        <Switch>
+                            <Route path="/auth">
+                                <AuthAppLazy onSignIn={() => setIsSignedIn(true)} />
+                            </Route>
+                            <Route path="/" component={MarketingAppLazy} />
+                        </Switch>
                     </Suspense>
                 </div>
             </StylesProvider>
--- packages/container/src/Components/AuthApp.js
+++ packages/container/src/Components/AuthApp.js
@@ -2,7 +2,7 @@ import React, { useRef, useEffect } from 'react'
 import { useHistory } from 'react-router-dom'
 import { mount } from 'auth/AuthApp'
 
-export default () => {
+export default ({ onSignIn }) => {
     const ref = useRef(null)
     const history = useHistory()
 
@@ -16,7 +16,8 @@ export default () => {
                 if (currentPathname !== pathname) {
                     history.push(pathname)
                 }
-            }
+            },
+            onSignIn
         })
 
         history.listen(onParentNavigate)
