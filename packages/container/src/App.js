876a6c72fecf5f73c82862094951c7031fc25dc5
Skip <skoppensboer@gmail.com>
2021-01-03 09:00:07 +0200

    Added styleClassProvider seperation

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,15 +1,22 @@
 import React from 'react'
 import { BrowserRouter } from 'react-router-dom'
+import { StylesProvider, createGenerateClassName } from '@material-ui/styles'
 import Header from './Components/Header'
 import MarketingApp from './Components/MarketingApp'
+
+const generateClassName = createGenerateClassName({
+    productionPrefix: 'co'
+})
 
 export default () => {
     return (
         <BrowserRouter>
-        <div>
-            <Header />
-            <MarketingApp />
-        </div>
+            <StylesProvider generateClassName={generateClassName}>
+                <div>
+                    <Header />
+                    <MarketingApp />
+                </div>
+            </StylesProvider>
         </BrowserRouter>
     )
 }
